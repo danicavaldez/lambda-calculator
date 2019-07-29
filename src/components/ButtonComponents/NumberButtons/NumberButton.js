aa9dbfe650533eb18a19700323be015eff7a1bc4
Danica Valdez <danicavaldez@Danicas-MacBook-Pro.local>
2019-07-29 11:39:21 -0700

    MVP completed

--- src/components/ButtonComponents/NumberButtons/NumberButton.js
+++ src/components/ButtonComponents/NumberButtons/NumberButton.js
@@ -1,9 +1,15 @@
 import React from "react";
+{/* Display a button element rendering the data being passed down from the parent container on props */}
 
-const NumberButton = () => {
+const NumberButton = ({ number }) => {
   return (
     <>
-      {/* Display a button element rendering the data being passed down from the parent container on props */}
+      {number === '0'
+        ? <button name='zero'>{ number }</button>
+        : <button>{ number }</button>
+      }
     </>
   );
 };
+
+export default NumberButton
